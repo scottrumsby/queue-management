eedbd0f69c61e52e7b966ff7e62ded1dac1829ff
Scott Rumsby <scott@olivewood.io>
2018-11-30 11:59:34 -0800

    modified the existing admin console to bring it into alignment with app design and moved control of the views into the front-end.  added a route for the admin console and moved the control of the GA pannel into the navigation menu

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -25,7 +25,12 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
 
   state: {
-    navigationVisible: false,
+    iframeLogedIn: false,
+    viewPortSizes: {
+      h: null,
+      w: null
+    },
+    navigationVisible: true,
     addModalForm: {
       citizen:'',
       comments: '',
@@ -228,6 +233,12 @@ export const store = new Vuex.Store({
   },
 
   actions: {
+    loginIframe(context) {
+      Axios(context).get('/login/').then( () => {
+        context.commit('setiframeLogedIn', true)
+      })
+    },
+
     flashServeNow(context, payload) {
       let flash = () => {
         if (!context.state.showServiceModal) {
@@ -1412,6 +1423,17 @@ export const store = new Vuex.Store({
 
     hideResponseModal(state) {
       state.showResponseModal = false
-    }
+    },
+
+    updateViewportSizes(state, attr) {
+      let key = Object.keys(attr)[0]
+      Vue.set(
+        state.viewPortSizes,
+        key,
+        attr[key]
+      )
+    },
+
+    setiframeLogedIn: (state, value) => state.iframeLogedIn = value,
   }
 })
